794840607ace51d5c982844f1b53bfe25491bb82
rodriguezst <2828844+rodriguezst@users.noreply.github.com>
2024-06-21 00:13:49 +0200

    update_fs.sh: unxz input file when needed

--- scripts/update_fs.sh
+++ scripts/update_fs.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-./scripts/image_mount.sh $1 rootfs
+IMG_FILE="$1"
+if [[ "$IMG_FILE" == *.xz ]]; then
+  unxz $IMG_FILE
+  IMG_FILE="${IMG_FILE%.xz}"
+fi
+./scripts/image_mount.sh $IMG_FILE rootfs
 chroot rootfs pacman -R linux61
 rsync -a overlay/ rootfs/
 # update-initramfs
