743fda347f3c7b318926e928a373739cb399eadc
rodriguezst <2828844+rodriguezst@users.noreply.github.com>
2024-06-21 11:15:43 +0200

    XZ resulting RootFS images

--- scripts/image_extract.sh
+++ scripts/image_extract.sh
@@ -23,37 +23,33 @@ echo "Partitions found in $IMG_FILE:"
 echo "$PARTITIONS"
 
 # Extract rootfs partition
-  PART_START=$(echo $ROOTFS_PARTITION | awk '{print $2}')
-  PART_END=$(echo $ROOTFS_PARTITION | awk '{print $3}')
-  PART_SIZE=$((PART_END - PART_START + 1))
-  OUTPUT_FILE="${IMG_FILE%.*}-rootfs.img"
+PART_START=$(echo $ROOTFS_PARTITION | awk '{print $2}')
+PART_END=$(echo $ROOTFS_PARTITION | awk '{print $3}')
+PART_SIZE=$((PART_END - PART_START + 1))
+OUTPUT_FILE="${IMG_FILE%.*}-rootfs.img.xz"
 
-  echo "Extracting RootFS partition..."
-  dd if="$IMG_FILE" of="$OUTPUT_FILE" bs=512 skip=$PART_START count=$PART_SIZE
-  #echo "Compressing RootFS partition..."
-  #xz $OUTPUT_FILE
-  
-  if [ $? -eq 0 ]; then
-    echo "RootFS Partition extracted to $OUTPUT_FILE"
-  else
-    echo "Error extracting RootFS partition"
-  fi
+echo "Extracting RootFS partition..."
+dd if="$IMG_FILE" bs=512 skip=$PART_START count=$PART_SIZE | xz > "$OUTPUT_FILE"
+
+if [ $? -eq 0 ]; then
+  echo "RootFS Partition extracted to $OUTPUT_FILE"
+else
+  echo "Error extracting RootFS partition"
+fi
 
 # Mount EFI partition
-  PART_START=$(echo $EFI_PARTITION | awk '{print $2}')
-  PART_END=$(echo $EFI_PARTITION | awk '{print $3}')
-  PART_SIZE=$((PART_END - PART_START + 1))
-  OUTPUT_FILE="${IMG_FILE%.*}-esp.img"
+PART_START=$(echo $EFI_PARTITION | awk '{print $2}')
+PART_END=$(echo $EFI_PARTITION | awk '{print $3}')
+PART_SIZE=$((PART_END - PART_START + 1))
+OUTPUT_FILE="${IMG_FILE%.*}-esp.img.xz"
 
-  echo "Extracting EFI partition..."
-  dd if="$IMG_FILE" of="$OUTPUT_FILE" bs=512 skip=$PART_START count=$PART_SIZE
-  #echo "Compressing EFI partition..."
-  #xz $OUTPUT_FILE
-  
-  if [ $? -eq 0 ]; then
-    echo "EFI Partition extracted to $OUTPUT_FILE"
-  else
-    echo "Error extracting EFI partition"
-  fi
+echo "Extracting EFI partition..."
+dd if="$IMG_FILE" bs=512 skip=$PART_START count=$PART_SIZE | xz > "$OUTPUT_FILE"
+
+if [ $? -eq 0 ]; then
+  echo "EFI Partition extracted to $OUTPUT_FILE"
+else
+  echo "Error extracting EFI partition"
+fi
 
 echo "Extraction complete."
