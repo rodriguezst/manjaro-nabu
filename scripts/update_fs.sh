15e8bec7876d7d703b7d5afceccbf687b17d4d40
rodriguezst <2828844+rodriguezst@users.noreply.github.com>
2024-07-08 18:32:53 +0200

    update_fs.sh: make some commands less verbose

--- scripts/update_fs.sh
+++ scripts/update_fs.sh
@@ -22,15 +22,15 @@ fi
 # Check if the system architecture is not aarch64 (ARM 64-bit)
 if ! uname -m | grep -q aarch64; then
   # Download the QEMU user static binary for aarch64 emulation
-  wget https://github.com/multiarch/qemu-user-static/releases/download/v7.2.0-1/qemu-aarch64-static
+  wget --no-verbose https://github.com/multiarch/qemu-user-static/releases/download/v7.2.0-1/qemu-aarch64-static
   # Install the QEMU binary to the rootfs directory with appropriate permissions
   install -m755 qemu-aarch64-static rootfs/
 
   # Register the QEMU binary with binfmt_misc to handle aarch64 binaries
-  echo ':aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7:\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/qemu-aarch64-static:' | tee /proc/sys/fs/binfmt_misc/register
+  echo ':aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7:\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/qemu-aarch64-static:' | tee /proc/sys/fs/binfmt_misc/register > /dev/null
 
   # Register aarch64 dynamic linker with binfmt_misc
-  echo ':aarch64ld:M::\x7fELF\x02\x01\x01\x03\x00\x00\x00\x00\x00\x00\x00\x00\x03\x00\xb7:\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/qemu-aarch64-static:' | tee /proc/sys/fs/binfmt_misc/register
+  echo ':aarch64ld:M::\x7fELF\x02\x01\x01\x03\x00\x00\x00\x00\x00\x00\x00\x00\x03\x00\xb7:\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/qemu-aarch64-static:' | tee /proc/sys/fs/binfmt_misc/register > /dev/null
 fi
 
 # Enter the chroot environment and remove the linux61 package using pacman
@@ -150,8 +150,8 @@ menuentry 'Manjaro ARM Setup' --class manjaro --class gnu-linux --class gnu --cl
 # If the system architecture is not aarch64, clean up the binfmt_misc registrations and QEMU binary
 if ! uname -m | grep -q aarch64; then
   # Unregister the aarch64 binfmt_misc handlers
-  echo -1 | tee /proc/sys/fs/binfmt_misc/aarch64
-  echo -1 | tee /proc/sys/fs/binfmt_misc/aarch64ld
+  echo -1 | tee /proc/sys/fs/binfmt_misc/aarch64 > /dev/null
+  echo -1 | tee /proc/sys/fs/binfmt_misc/aarch64ld > /dev/null
   # Remove the QEMU binary from the rootfs and current directories
   rm rootfs/qemu-aarch64-static
   rm qemu-aarch64-static
